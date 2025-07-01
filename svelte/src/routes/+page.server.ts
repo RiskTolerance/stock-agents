b30e16aea3a15bb764619f58101ca2987e67ea77
RiskTolerance <isaac.druin@gmail.com>
2025-07-01 14:16:52 -0500

    replace chartjs with layerchart

--- svelte/src/routes/+page.server.ts
+++ svelte/src/routes/+page.server.ts
@@ -7,13 +7,13 @@ export const load: PageServerLoad = async () => {
 	// const data = await createFmpApi(FMP_API_KEY).Analyst.analystPriceTarget('AAPL');
 	const fmpApi = createFmpApi(FMP_API_KEY);
 	const priceChartData = await fmpApi.Chart.light('AAPL', {
-		from: dayjs().subtract(30, 'day').format('YYYY-MM-DD') as unknown as Date,
+		from: dayjs().subtract(90, 'day').format('YYYY-MM-DD') as unknown as Date,
 		to: dayjs().format('YYYY-MM-DD') as unknown as Date // TODO: change types to string
 	});
 	// since the api returns the latest data first, we need to reverse the array for the chart to render in chronological order
 	priceChartData.reverse();
 	const priceChart = priceChartData.map((item) => ({
-		date: item.date,
+		date: new Date(item.date),
 		price: item.price
 	}));
 	return {
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,68 +1,60 @@
 <script lang="ts">
+	// import dayjs from 'dayjs';
+
+	import { AreaChart } from 'layerchart';
+	import { curveLinear } from 'd3-shape';
+	import { scaleUtc, scaleTime, scaleLog, scaleLinear } from 'd3-scale';
+	import * as Chart from '$lib/components/ui/chart';
+
 	let { data } = $props();
-	import { onMount } from 'svelte';
-	import Chart, { type ChartItem } from 'chart.js/auto';
-	let myChart: HTMLCanvasElement;
 	let symbol = $state('AAPL');
 
-	onMount(() => {
-		const lineChart = new Chart(myChart, {
-			type: 'line',
-			data: {
-				labels: data.priceChart.map((item) => item.date),
-				datasets: [
-					{
-						label: 'AAPL Price',
-						data: data.priceChart.map((item) => item.price)
-					}
-				]
-			},
-			options: {
-				color: 'rgb(78, 197, 241)',
-				borderColor: 'rgba(78, 197, 241, 0.5)',
-				backgroundColor: 'rgb(13, 242, 201)',
-				scales: {
-					x: {
-						ticks: {
-							color: 'rgba(238, 238, 238, 0.9)'
-						},
-						grid: {
-							color: 'rgba(238, 238, 238, 0.1)'
-						}
-					},
-					y: {
-						ticks: {
-							color: 'rgba(238, 238, 238, 0.9)'
-						},
-						grid: {
-							color: 'rgba(238, 238, 238, 0.7)'
-						}
-					}
-				},
-				plugins: {
-					legend: {
-						display: true,
-						labels: {
-							color: 'rgba(238, 238, 238, 0.9)'
-						}
-					}
-				}
-			}
-		});
-	});
+	const chartConfig = {
+		price: {
+			label: 'Price',
+			color: 'rgb(78, 197, 241)'
+		}
+	} satisfies Chart.ChartConfig;
 </script>
 
-<!-- {#if data}
+{#if data}
 	<pre>{JSON.stringify(data, null, 2)}</pre>
-{/if} -->
+{/if}
 
-<div class="p-8 bg-slate-900 w-fit flex flex-col gap-4">
+<div class="p-8 bg-slate-900 w-fit grid grid-cols-2 gap-4">
 	<div class="flex flex-col gap-1">
 		<label class="text-xs font-medium" for="symbol">Ticker Symbol</label>
 		<input id="symbol" class="w-24 bg-white rounded-md p-2" type="text" bind:value={symbol} />
 	</div>
-
 	<div class="w-[800px]">
-		<canvas bind:this={myChart} id="myChart"></canvas>
+		<Chart.Container config={chartConfig}>
+			<AreaChart
+				data={data.priceChart}
+				x="date"
+				xScale={scaleTime()}
+				yScale={scaleLinear()}
+				series={[{ key: 'price', label: 'Price', color: chartConfig.price.color }]}
+				axis="x"
+				props={{
+					area: {
+						curve: curveLinear,
+						'fill-opacity': 0.5,
+						line: { class: 'stroke-1' },
+						motion: 'tween'
+					},
+					xAxis: {
+						format: (v: Date) => v.toLocaleDateString('en-US', { month: 'short' })
+					}
+				}}
+				y="price"
+			>
+				{#snippet tooltip()}
+					<Chart.Tooltip
+						labelFormatter={(v: Date) => v.toLocaleDateString('en-US', { month: 'long' })}
+						indicator="line"
+					/>
+				{/snippet}
+			</AreaChart>
+		</Chart.Container>
 	</div>
 </div>
